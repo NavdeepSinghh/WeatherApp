d143a1ed9120184ce90434c75dda9dd5064cfc6b
Navdeep  Singh <navdeep.singh@sonartechnologies.com>
2017-05-29 16:47:24 +1000

    Model + ViewModel implemented

---
+++ WeatherAppWH/Models/CurrentWeather.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentWeather.swift
+//  WeatherAppWH
+//
+//  Created by Navdeep  Singh on 5/29/17.
+//  Copyright © 2017 Navdeep. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeather{
+    let temperature : Double
+    let humidity : Double
+    let precipitationProb : Double
+    let summary : String
+    let icon : String
+}
