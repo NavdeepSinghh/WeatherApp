6de46fec19ccb51d239639fb5aef07df162543aa
Navdeep  Singh <navdeep.singh@sonartechnologies.com>
2017-05-29 14:46:18 +1000

    WeatherView Controller created + icons added to assets

---
+++ WeatherAppWH/Controllers/WeatherVC.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherVC.swift
+//  WeatherAppWH
+//
+//  Created by Navdeep  Singh on 5/29/17.
+//  Copyright © 2017 Navdeep. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+    
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    @IBOutlet weak var currentHumidityLabel: UILabel!
+    @IBOutlet weak var currentPrecipitationLabel: UILabel!
+    @IBOutlet weak var currentWeatherIcon: UIImageView!
+    @IBOutlet weak var currentSummaryLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
